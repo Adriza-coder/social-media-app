73063e98735351c18c1904ccf058435a5c9865f7
Adriza-coder <ddaskol86@gmail.com>
2025-11-07 19:26:32 +0530

    Add files via upload

---
+++ frontend/src/Background.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Background.css"; // we’ll create this next
+
+const Background = () => {
+  return (
+    <div className="bubbles">
+      <span></span><span></span><span></span><span></span><span></span>
+      <span></span><span></span><span></span><span></span><span></span>
+    </div>
+  );
+};
+
+export default Background;
